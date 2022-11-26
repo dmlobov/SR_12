51811fe303c01e808ca20a15154528b7fc1f8a6f
dmlob <dmlob@DESKTOP-0VEH751>
2022-11-26 17:19:29 +0300

    Добавьте файлы проекта.

---
+++ Task1/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    static class Circle
+    {
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r*r;
+        }
+        static public string GetBelonging(double x, double y, double r)
+        {
+            if (Math.Sqrt((x * x) + (y * y)) < r)
+                return "Точка лежит внутри окружности";
+            else
+            {
+                return "Точка не лежит внутри окружности";
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ведите координаты точки x, y");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double lendth = Circle.GetLength(r);
+            double square = Circle.GetSquare(r);
+            string belongin = Circle.GetBelonging(x, y, r);
+            Console.WriteLine("Длина окружности {0:0.00}, площадь круга {1:0.00}", lendth, square);
+            Console.WriteLine(belongin);
+            Console.ReadKey();
+
+
+        }
+    }
+}
